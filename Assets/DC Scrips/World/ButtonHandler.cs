be00ac6f39ef3232da24aea7eb4bce9e32dfd267
Cameron Denton <cdentonPD@gmail.com>
2017-07-28 17:10:43 -0400

    Tweaking cameras

--- Assets/DC Scrips/World/ButtonHandler.cs
+++ Assets/DC Scrips/World/ButtonHandler.cs
@@ -7,10 +7,19 @@ public class ButtonHandler : MonoBehaviour {
     public Canvas buttonsCanvas;
     public Transform playerOne;
     public Transform playerTwo;
+    public Transform playerThree;
+    public Transform playerFour; 
     Vector3 playerOneSpawn;
     Vector3 playerTwoSpawn;
+    Vector3 playerThreeSpawn;
+    Vector3 playerFourSpawn; 
     public GameObject playerOneObj;
+    public GameObject playerOneCornerObj;
     public GameObject playerTwoObj;
+    public GameObject playerTwoCornerObj;
+    public GameObject playerThreeObj;
+    public GameObject playerThreeCornerObj;
+    public GameObject playerFourObj; 
     public GameObject singlePlayerObj;
     public Camera menuCamera;
 
@@ -35,5 +44,25 @@ public class ButtonHandler : MonoBehaviour {
         Instantiate(playerOneObj, playerOneSpawn, Quaternion.identity);
         Instantiate(playerTwoObj, playerTwoSpawn, Quaternion.identity);
          
+    }
+
+    public void PlayerThreeSelected()
+    {
+        buttonsCanvas.enabled = false;
+        menuCamera.enabled = false;
+        Instantiate(playerThreeObj, playerThreeSpawn, Quaternion.identity);
+        Instantiate(playerTwoCornerObj, playerTwoSpawn, Quaternion.identity);
+        Instantiate(playerOneCornerObj, playerOneSpawn, Quaternion.identity);
+    }
+
+    public void PlayerFourSelected()
+    {
+        buttonsCanvas.enabled = false;
+        menuCamera.enabled = false;
+        Instantiate(playerOneCornerObj, playerOneSpawn, Quaternion.identity);
+        Instantiate(playerTwoCornerObj, playerTwoSpawn, Quaternion.identity);
+        Instantiate(playerThreeCornerObj, playerThreeSpawn, Quaternion.identity);
+        Instantiate(playerFourObj, playerFourSpawn, Quaternion.identity);
+
     }
 }
